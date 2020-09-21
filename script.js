84c426b3aa4306eb10c2b67e20588c12d0d32efe
Alex Nordin <apnordin@gmail.com>
2020-09-21 19:52:11 -0400

    Added password length restrictions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function generate(){
 
-    // alert("Upper case letters?");
+    // Prompts for password criteria
     var upperCase = confirm("Do you want your password to contain upper case letters?");
     
     var lowerCase = confirm("Do you want your password to contain lower case letters?");
@@ -9,9 +9,20 @@ function generate(){
 
     var specialCharacters = confirm("Do you want your password to contain special characters?")
 
-    var passwordLength = prompt("Enter the desired length of your password.");
-    
+    var passwordLength = prompt("Enter the desired length of your password. Minimum 8, maximum 128.");
 
+    // Password length restrictions
+    if (passwordLength < 8){
+        alert("Password length must be between 8 and 128.");
+        return;
+    }
+
+    if (passwordLength > 128){
+        alert ("Password length must be between 8 and 128.");
+        return;
+    }
+    
+    // Set sub-values based off each prompt response. Ok = values, cancel = null
     if (upperCase === true){
         var valuesU = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     } else {var valuesU = ""}
@@ -28,14 +39,18 @@ function generate(){
         var SCN = "!@#$%^&*()_+"
     } else {var SCN = ""}
     
+    // Combine sub-values to create list of all possible password characters called "values"
     var values = valuesU + valuesL + valuesN + SCN;
 
+    // Set password to null
     let password = "";
 
+    // Generate password and add to null
     for(var i=0; i<=passwordLength - 1; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     } 
 
+    // Insert generated password into display box
     document.getElementById("display").value = password;
 }
 
